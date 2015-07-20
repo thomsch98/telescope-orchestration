1f2caa3da959f3ff988d28142bf155a934c81ecd
Thomas Schuering <thomas.schuering@haufe-lexware.com>
2015-07-20 15:15:15 +0200

    Modified meteor/telscope installation sequence in Dockerfile to cirvumvent the need of "root" as service owner.

--- telescope-on-docker/run.sh
+++ telescope-on-docker/run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-## git pull
+
+# Make sure it is NOT root that runs our script
+if [[ $EUID -eq 0 ]]; then
+   echo "This script must NOT be run as root" 1>&2
+   exit 1
+fi
+
+cd /homes/telescope/Telescope
+
 meteor
